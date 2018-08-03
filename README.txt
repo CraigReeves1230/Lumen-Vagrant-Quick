b721a22da1468ab17d8c3cdb857e40d9668decdb
Craig Reeves <craig.reeves@Craigs-MacBook-Pro.local>
2018-08-02 21:57:37 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,12 @@
+This was tested and designed using Lumen 5.5.
+
+The address you will put in your address bar is simply localhost.
+
+1. Clone or download a new Lumen project from https://www.github.com/laravel/lumen
+
+2. Take every one of the files in this repository EXCEPT for this README, .vagrant and .get and copy them into your Lumen's project's folder. 
+Make sure they're in the project's main folder.
+
+3. Run vagrant up in your command line and it should provision without any problems.
+
+4. Migrate the database if you wish and you're all set!
---
+++ provision.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+echo "-- Updating Apt-Get --"
+sudo apt-get update
+
+echo "-- Installing MySQL --"
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+sudo apt-get -y install mysql-server
+
+echo "-- Installing node, git, Java and other dependencies --"
+sudo apt-get -y install nodejs-legacy vim lsof git-core default-jre curl zlib1g-dev build-essential
+libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev
+python-software-properties libffi-dev
+
+echo "-- Installing NGinX --"
+sudo apt-get install -y nginx
+sudo ufw allow 'Nginx HTTP'
+
+echo "-- Installing PHP and Composer --"
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y php7.1
+sudo apt-cache search php7.1
+
+sudo apt install -y php7.1-fpm php7.1-mbstring php7.1-xml php7.1-mysql php7.1-common php7.1-gd php7.1-json php7.1-cli php7.1-curl
+sudo apt-get install -y php7.1-zip
+sudo systemctl start php7.1-fpm
+
+echo "-- Install and Start Laravel --"
+cd /vagrant
+
+# composer install
+wget https://getcomposer.org/download/1.6.3/composer.phar
+sudo chmod 755 composer.phar
+sudo mv composer.phar /usr/local/bin/composer
+
+#Install Laravel
+composer install
+
+# mysql config
+sudo rm /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo ln -s /vagrant/mysql_cnf /etc/mysql/mysql.conf.d/mysqld.cnf 
+sudo service mysql restart
+
+sudo rm /etc/nginx/sites-available/default
+sudo rm /etc/nginx/nginx.conf
+sudo ln -s /vagrant/nginx-default /etc/nginx/sites-available/default
+sudo ln -s /vagrant/nginx-config /etc/nginx/nginx.conf
+sudo service nginx restart
+sudo service nginx reload
+sudo cp preset-env .env
+sudo rm preset-env
+cd ..
+sudo chown -R www-data /vagrant
+
+echo "-- Provisioning Complete! You will need perform your first migration. --"
+
+
+
+
+
