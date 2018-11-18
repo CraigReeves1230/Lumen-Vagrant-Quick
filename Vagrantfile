437c218dc6a992fc1a027b681993b93a8b69ebf7
craig.reeves <craigr@ternio.io>
2018-11-18 00:00:49 -0500

    made some tweaks

--- Vagrantfile
+++ Vagrantfile
@@ -5,5 +5,8 @@ Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.network "private_network", type: "dhcp"
   config.vm.provision "shell", path: "provision.sh"
+  config.vm.provider "virtualbox" do |v|
+          v.memory = 2048
+      end
 end
 
--- provision.sh
+++ provision.sh
@@ -3,46 +3,64 @@
 echo "-- Updating Apt-Get --"
 sudo apt-get update
 
-echo "-- Installing MySQL --"
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
+echo "-- Installing PostgreSQL --"
+sudo apt-get -y install wget postgresql postgresql-contrib
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
 echo "-- Installing node, git, Java and other dependencies --"
 sudo apt-get -y install nodejs-legacy vim lsof git-core default-jre curl zlib1g-dev build-essential
 libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev
 python-software-properties libffi-dev
 
+echo "-- Installing Redis --"
+sudo apt-get update
+sudo apt-get -y install build-essential tcl
+cd /tmp
+curl -O http://download.redis.io/redis-stable.tar.gz
+tar xzvf redis-stable.tar.gz
+cd redis-stable
+make
+sudo make install
+
 echo "-- Installing NGinX --"
 sudo apt-get install -y nginx
 sudo ufw allow 'Nginx HTTP'
 
-echo "-- Installing PHP and Composer --"
+echo "-- Installing PHP --"
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y php7.1
-sudo apt-cache search php7.1
+sudo apt install -y php7.3-fpm php7.3-mbstring php7.3-xml php7.3-mysql php7.3-fpm php7.3-common php7.3-gd php7.3-json php7.3-cli php7.3-curl php-zmq php-apcu php-memcached \
+php-solr php-mongodb php7.3-bcmath php7.3-pgsql unzip php7.3-dev php7.3- php7.3-opcache php-zmq php-stomp php-imagick php7.3-zip php-pear php7.3-dev php-mongodb
 
-sudo apt install -y php7.1-fpm php7.1-mbstring php7.1-xml php7.1-mysql php7.1-common php7.1-gd php7.1-json php7.1-cli php7.1-curl
-sudo apt-get install -y php7.1-zip
-sudo systemctl start php7.1-fpm
+echo "-- Installing BeanstalkD --"
+sudo apt-get -y install beanstalkd
+sudo apt-get -y install cron
+
+echo "-- Install PHP Redis --"
+cd /
+sudo apt-get -y install gcc make autoconf libc-dev pkg-config
+cd /tmp
+sudo wget https://github.com/phpredis/phpredis/archive/master.zip -O phpredis.zip
+sudo unzip -o /tmp/phpredis.zip && mv /tmp/phpredis-* /tmp/phpredis && cd /tmp/phpredis && phpize && ./configure && make && sudo make install
+sudo touch /etc/php/7.3/mods-available/redis.ini && echo extension=redis.so > /etc/php/7.3/mods-available/redis.ini
+sudo ln -s /etc/php/7.3/mods-available/redis.ini /etc/php/7.3/fpm/conf.d/redis.ini
+sudo ln -s /etc/php/7.3/mods-available/redis.ini /etc/php/7.3/cli/conf.d/redis.ini
+sudo apt -y install php7.3-bcmath
+sudo ./configure --enable-redis-igbinary
+sudo service php7.3-fpm restart
 
 echo "-- Install and Start Laravel --"
 cd /vagrant
 
 # composer install
-wget https://getcomposer.org/download/1.6.3/composer.phar
+sudo wget https://getcomposer.org/download/1.6.3/composer.phar
 sudo chmod 755 composer.phar
 sudo mv composer.phar /usr/local/bin/composer
 
 #Install Laravel
+cd /vagrant
 composer install
-
-# mysql config
-sudo rm /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo ln -s /vagrant/mysql_cnf /etc/mysql/mysql.conf.d/mysqld.cnf 
-sudo service mysql restart
 
 sudo rm /etc/nginx/sites-available/default
 sudo rm /etc/nginx/nginx.conf
